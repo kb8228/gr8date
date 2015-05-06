21bf5183e2b98879bf7ef989fbd63583e12427cb
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-05-05 17:00:11 -0700

    Specs Working

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,9 +1,9 @@
 class Plan < ActiveRecord::Base
   belongs_to :user
 
-  validates :date, presence:true
-  validates :time, presence:true
-  validates :location, presence:true
-  validates :user_id, presence:true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :location, presence: true
+  validates :user_id, presence: true
 
 end
--- spec/models/plans_spec.rb
+++ spec/models/plans_spec.rb
@@ -2,26 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
     it "should have a date" do
-        plan = FactoryGirl.create(:plan, date: nil)
-        p plan
+        plan = FactoryGirl.build_stubbed(:plan, date: nil)
         expect(plan).to be_invalid
     end
 
     it "should have a time" do
-        plan = FactoryGirl.create(:plan, time: nil)
-        p plan
+        plan = FactoryGirl.build_stubbed(:plan, time: nil)
         expect(plan).to be_invalid
     end
 
     it "should have a location" do
-        plan = FactoryGirl.create(:plan, location: nil)
-        p plan
+        plan = FactoryGirl.build_stubbed(:plan, location: nil)
         expect(plan).to be_invalid
     end
 
     it "should have a user" do
-        plan = FactoryGirl.create(:plan, user_id: nil)
-        p plan
+        plan = FactoryGirl.build_stubbed(:plan, user_id: nil)
         expect(plan).to be_invalid
     end
 end
