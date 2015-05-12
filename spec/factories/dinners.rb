2b9f14291f50a142663627300ba5f9f8d1cbc482
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-05-11 18:38:47 -0700

    Added Dinners Table

---
+++ spec/factories/dinners.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :dinner do
+    name "MyString"
+address "MyString"
+phone "MyString"
+type ""
+cuisine "MyString"
+datetime "2015-05-11 18:37:38"
+alcohol "MyString"
+  end
+
+end
