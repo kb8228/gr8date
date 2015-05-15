0033b9aa0ca24fd8c790ce4f919bff684aad45c3
Ksenia Smith <smithkseniav@gmail.com>
2015-05-14 17:15:09 -0700

    fixed jQ click on event <li>

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,23 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 $(document).ready(function(){
-
-  var API_BASE = "/api/events";
-
-  function addEvent(jqElem, data){
-    var eventName = data.event_name;
-    var eventDate = data.event_date;
-    var eventTime = data.event_time;
-    var venueName = data.venue_name;
-    var venueAddress = data.venue_address;
-    var venuePhone = data.phone;
-    var eventCategory = data.category;
-    var venueUrl = data.venue_url;
-
-    var listItem = "<div class='item-box'>";
-    listItem += "<h3>eventName</h3>"
-    console.log(eventDate);
-    console.log(eventTime);
-  };
+  var API_BASE = "/api/events/";
 
   var json = $.ajax({
     datatype: 'json',
@@ -30,13 +13,36 @@ $(document).ready(function(){
   json.done(function(data){
     var i;
     for(i=0; i<6; i++){
-      addEvent( $('#event-list'), i );
+      addEvent( $('#event-list'), data[i] );
     }
   });
 
   // for when json fails:
   json.fail(function(data){
     $('event-section').html("<h2>Data failed to load.</h2>");
+  });
+  console.log(json);
+
+  function addEvent(jqElem, data){
+    var eventId = data.id;
+    var eventName = data.event_name;
+    var eventDate = data.event_date;
+    var eventTime = data.event_time;
+    var venueName = data.venue_name;
+    var venueAddress = data.venue_address;
+    var venuePhone = data.phone;
+    var eventCategory = data.category;
+    var venueUrl = data.venue_url;
+
+    var listItem = "<div class='item-box'" + eventId + ">";
+    listItem += "<h3>" + eventName + "</h3></div>"; //finish this div
+
+    jqElem.append("<li id='event" + eventId + "'>" + listItem + "</li>");   
+  }; // AddEvent END
+
+  $("#event-list").on('click', 'li', function(){
+    var id = $(this).attr('id');
+    console.log(id);
   });
 
 }); //doc ready ends
