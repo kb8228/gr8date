624906e7c14d8090e3465b2dbbe714d04fb1ef1a
Ksenia Smith <smithkseniav@gmail.com>
2015-05-15 14:45:14 -0700

    made picking event work

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,59 +2,37 @@ User.destroy_all #clears out database of all data / cleans slate
 #array of Users
 users = User.create([
 
-  
-
-  {
+  {id: 1,
    name: "Nick",
    email: "nick@example.com",
    password: "password",
    password_confirmation: "password",
   },
-
-  {
-
-  {
-
+  {id: 2,
    name: "Zack",
    email: "zack@example.com",
    password: "password",
    password_confirmation: "password"
   },
-
-  {
-=======
-  {
-
+  {id: 3,
    name: "Ksenia",
    email: "ksenia@example.com",
    password: "password",
    password_confirmation: "password"
   },
-
-  {
-
-  {
-
+  {id: 4,
    name: "Shawn",
    email: "shawn@example.com",
    password: "password",
    password_confirmation: "password"
   },
-
-  {
-
-  {
-
+  {id: 5,
    name: "Glenn",
    email: "glenn@example.com",
    password: "password",
    password_confirmation: "password"
   },
-
-  {
-
-  {
-
+  {id: 6,
    name: "Jimmy",
    email: "jimmy@example.com",
    password: "password",
